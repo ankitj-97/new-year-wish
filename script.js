307ee184c0d180ac55d3570d02982ef7b5e580b8
ankitj-97 <84343460+ankitj-97@users.noreply.github.com>
2023-12-31 18:25:03 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,19 @@
-alert(`Answer 1: ${answer1}, Answer 2: ${answer2}, Answer 3: ${answer3}`);
+
   
   function checkAnswers() {
     // Your logic to check the answers goes here
     const answer1 = document.getElementById("answer1").value.toLowerCase();
     const answer2 = document.getElementById("answer2").value.toLowerCase();
     const answer3 = document.getElementById("answer3").value.toLowerCase();
-  
+    
+    alert(`Answer 1: ${answer1}, Answer 2: ${answer2}, Answer 3: ${answer3}`);
+
     // You can customize the correct answers
     const correctAnswer1 = "yeh dosti hum nahin todenge";
     const correctAnswer2 = "jag soona soona lage";
     const correctAnswer3 = "dil toh bachcha hai";
-  
+    
+
     if (answer1 === correctAnswer1 && answer2 === correctAnswer2 && answer3 === correctAnswer3) {
       window.location.href = "message.html";
     } else {
