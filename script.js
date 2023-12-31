68e549c081de13262a99bd014c9a788005e82bb5
ankitj-97 <84343460+ankitj-97@users.noreply.github.com>
2023-12-31 17:51:05 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+
+  
+  function checkAnswers() {
+    // Your logic to check the answers goes here
+    const answer1 = document.getElementById("answer1").value.toLowerCase();
+    const answer2 = document.getElementById("answer2").value.toLowerCase();
+    const answer3 = document.getElementById("answer3").value.toLowerCase();
+  
+    // You can customize the correct answers
+    const correctAnswer1 = "Yeh Dosti Hum Nahin Todenge";
+    const correctAnswer2 = "Jag Soona Soona Lage";
+    const correctAnswer3 = "Dil Toh Bachcha Hai";
+  
+    if (answer1 === correctAnswer1 && answer2 === correctAnswer2 && answer3 === correctAnswer3) {
+      window.location.href = "message.html";
+    } else {
+      alert("Incorrect answers. Try again!");
+    }
+  }
+  
+  function goBack() {
+    window.location.href = "index.html";
+  }
+
+  let audio = document.getElementById("backgroundAudio");
+
+function playAudio() {
+  audio.play();
+}
+
+function pauseAudio() {
+  audio.pause();
+}
